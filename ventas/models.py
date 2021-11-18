a3e2ae31f13fe88cda12aecd1409ca6b5adb5ed9
Leal31 <lealemanuel31@protonmail.com>
2021-11-18 14:35:54 +0000

    cambios model

--- ventas/models.py
+++ ventas/models.py
@@ -3,11 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Ventas(models.Model):
-
-    cedula_cliente=models.IntegerField(max_length=50, null=True)
-    codigo_venta=models.IntegerField(max_length=50, null=True)
-    ivacompra=models.IntegerField(max_length=50, null=True)
-    nitproveedor=models.IntegerField(max_length=50, null=True)
-    nombre_producto=models.CharField(max_length=255, null=True)
-    precio_compra=models.IntegerField()
-    precio_venta=models.IntegerField()
+    codigo_venta=models.IntegerField(primary_key=True)
+    cedula_cliente=models.IntegerField()
+    nitproveedor=models.IntegerField()
+    nombre_producto=models.CharField(max_length=255)
+    cantidad_producto=models.IntegerField()
--- ventas/serializer.py
+++ ventas/serializer.py
@@ -4,4 +4,4 @@ from .models import *
 class VentasSerializers(serializers.ModelSerializer):
     class Meta:
         model = Ventas
-        fields = ['cedula_cliente', 'codigo_venta', 'ivacompra', 'nitproveedor', 'nombre_producto', 'precio_compra', 'precio_venta']
+        fields = ['codigo_venta', 'cedula_cliente','nitproveedor', 'nombre_producto', 'cantidad_producto']
