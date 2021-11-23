46bb06190c5db726fb91dd2c9e401529be462fe8
Leal31 <lealemanuel31@protonmail.com>
2021-11-23 17:50:05 -0500

    base de datos

--- ventas/models.py
+++ ventas/models.py
@@ -3,13 +3,11 @@ from django import forms
 # Create your models here.
 class DetalleVentas(models.Model):
     cantidad_producto = models.IntegerField(null=True)
-    codigo_producto = models.IntegerField(null=True)
+    codigo_producto = models.IntegerField(primary_key=True, unique=False)
     valor_total = models.FloatField(null=True)
     valor_venta = models.FloatField(null=True)
     valoriva = models.FloatField()
     objects = models.DjongoManager()
-    class Meta:
-        abstract = True
 
 class DetalleVentasForm(forms.ModelForm):
 
