fb39c9a52f3f52093ea9feb800ac857440a11f2b
Leal31 <lealemanuel31@protonmail.com>
2021-11-24 11:58:15 -0500

    ya

--- ventas/models.py
+++ ventas/models.py
@@ -2,24 +2,24 @@ from djongo import models
 from django import forms
 # Create your models here.
 class DetalleVentas(models.Model):
+    id = models.ObjectIdField()
     cantidad_producto = models.IntegerField(null=True)
-    codigo_producto = models.IntegerField(primary_key=True, unique=False)
+    codigo_producto = models.IntegerField(null=True)
     valor_total = models.FloatField(null=True)
     valor_venta = models.FloatField(null=True)
     valoriva = models.FloatField()
-    objects = models.DjongoManager()
 
 class DetalleVentasForm(forms.ModelForm):
 
     class Meta:
         model = DetalleVentas
         fields = (
-            'cantidad_producto', 'codigo_producto', 'valor_total', 'valor_venta', 'valoriva'
+           'id','cantidad_producto', 'codigo_producto', 'valor_total', 'valor_venta', 'valoriva'
                 )
 
 class Ventas(models.Model):
-    cedula_cliente = models.IntegerField(primary_key=True, unique=False)
-    codigo_venta = models.IntegerField(null=True) 
+    cedula_cliente = models.IntegerField(null=True)
+    codigo_venta = models.IntegerField(primary_key=True) 
     detalle_venta = models.ArrayField(model_container=DetalleVentas, model_form_class = DetalleVentasForm, null=True)
     iva_venta = models.FloatField(null=True)
     total_venta = models.FloatField(null=True)
