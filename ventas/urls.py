1ab85b6ac984a8a071ea9b42d353aeed4411806d
Leal31 <lealemanuel31@protonmail.com>
2021-11-26 14:04:22 -0500

    por cliente

--- ventas/urls.py
+++ ventas/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 router = routers.DefaultRouter()
 router.register(r'ventas', views.VentasViewSet, basename = 'ventas')
-
+router.register(r'ventaporcliente', views.ClienteViewSet, basename ='lista')
 
 urlpatterns = [
     path('', include(router.urls)),
--- ventas/views.py
+++ ventas/views.py
@@ -1,10 +1,22 @@
 from rest_framework import viewsets
 from .models import *
 from .serializer import *
+from rest_framework.response import Response
 
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 class VentasViewSet(viewsets.ModelViewSet):
     queryset = Ventas.objects.all()
     serializer_class = VentasSerializers
 
+
+class ClienteViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Ventas.objects.all()
+        serializer_class = VentasSerializers(queryset, many=True)
+        return Response(serializer_class.data)
+    def retrieve(self, request, pk=None):
+        queryset = Ventas.objects.filter(cedula_cliente=pk)
+        serializer = VentasSerializers(queryset, many=True)
+        return Response(serializer.data)
